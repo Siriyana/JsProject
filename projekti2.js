513ca2b6a71e257c65ffc206307c84991c074a56
Veera Kettunen <veera.kettunen@student.laurea.fi>
2024-05-07 14:24:19 +0300

    Projekti2

--- projekti2.js
+++ projekti2.js
@@ -4,40 +4,53 @@ const hae_paikka_btn = document.getElementById('asema');
 //Dynamic events fot buttons
 hae_paikka_btn.addEventListener('click', haePaikkaJSON);
 
-function haePaikkaJSON(e) {
-    e.preventDefault();
-    var asema = document.getElementById('aseman_haku').value;
+//alustetaan muuttuja, johon tallenetaan asemannimet, niin että sitä voidaan hyödyntää myös toisessa funktiossa
+let asemaNimet = null;
 
-    var haku = new XMLHttpRequest();
+
+//HAKU-FUNKTIO
+//Haetaan käyttäjän antamalla aseman nimellä löytyykö sen nimistä VR:n asemaa ja jos ei löydy, niin ilmoittaa
+function haePaikkaJSON(e) {
+    e.preventDefault(); 
+    var asema = document.getElementById('aseman_haku').value; //muuttujaan tallennetaan käyttäjän syöttämä tieto
+
+    var haku = new XMLHttpRequest(); 
 
     haku.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200){
             var jsonDocument = haku.responseText;
             var jsData = JSON.parse(jsonDocument);
+            asemaNimet = jsData; //tallennetaan haetut asematiedot muuttujaan, jota tarvitaan myöhemmin tämän funktion ulkopuolella
 
-            var found = false;
+            //alustetaan muuttuja, että onko asema löytynyt
+            var found = false; 
 
+            //käydään läpi haetut aesmatiedot asema kerrallaan ja tarkistetaan löytyykö haettu asema
             for (var i = 0; i < jsData.length; i++){
                 var asemaTiedot = jsData[i];
 
-                var stationName = asemaTiedot.stationName.toLowerCase();
+                var stationName = asemaTiedot.stationName.toLowerCase(); //muutetaan asemanimet pieniksi kirjaimeksi ja seuraavalla rivillä myös hakuasema, jotta vertailu onnistuu
                 if (stationName.includes(asema.toLowerCase())) {  
                     found = true;
-                    var lyhenne = asemaTiedot.stationShortCode;
+                    var lyhenne = asemaTiedot.stationShortCode; //kun asema löytynyt, niin haetaan aseman lyhennetieto, jota tarvitaan seuraavassa funktiossa
     
-                    haeAsemanJunat(lyhenne);
-                    findLocation(asema);
+                    haeAsemanJunat(lyhenne); //kutsutaan fuktio junien hakemiseksi
+                    findLocation(asema); //kutsutaan fuktio paikallisen säätiedon hakemiseksi
     
+                    //kun asema löytynyt, niin laitetaan näkyviksi juna- ja säätieto osiot
                     var junaTiedot = document.getElementById('junaTiedot');
-                    junaTiedot.style.display = 'block';
+                    junaTiedot.style.display = 'flex';
+                    saaTieto.style.display = 'flex';
                     var otsikko = document.getElementById('otsikko');
-                    otsikko.innerHTML = '<h3> ' + asema + ': lähtevät junat</h3>';
+                    //nimetään otsikko, johon tulostuu haetun aseman nimi isoilla kirjaimilla
+                    otsikko.innerHTML = '<h3> ' + asema.toUpperCase() + ': Lähtevät junat</h3>';
                         
                     break;
                 }
 
             }
             
+            //jos asemaa ei löytynyt, niin ikkunaan tulee ilmoitus siitä
             if (!found) {
                 alert("Hakemaasi asemaa ei löytynyt");
 
@@ -50,10 +63,12 @@ function haePaikkaJSON(e) {
 }
 
 
+//haetaan asemalta lähtevät junat ja niiden tiedot
+
 function haeAsemanJunat(lyhenne) {
-    console.log(lyhenne);
     var junaHaku = new XMLHttpRequest();
-    var junaURL = "https://rata.digitraffic.fi/api/v1//live-trains/station/" + lyhenne + "?minutes_before_departure=30&minutes_after_departure=0&minutes_before_arrival=0&minutes_after_arrival=0";
+    //määritelty api-kutsuun, että haetaan seuraavan 60min aikana asemalta lähtevät junat, aikaa olisi voinut määritellä miten vaan ja olisi voinut hakea myös saapuvat junat
+    var junaURL = "https://rata.digitraffic.fi/api/v1//live-trains/station/" + lyhenne + "?minutes_before_departure=60&minutes_after_departure=0&minutes_before_arrival=0&minutes_after_arrival=0";
     junaHaku.open("GET", junaURL, true);
     junaHaku.send();
 
@@ -63,15 +78,15 @@ function haeAsemanJunat(lyhenne) {
             var junaData = JSON.parse(junaJson);
             console.log(junaData);
 
-            var taulukko = "<table border='1'><tr><th>Lähtöaika</th><th>Juna</th><th>Junan päämäärä</th></tr>";
-    
-
+            //luodaan lista asemalla pysähtyvistä junista ja niiden tiedoista, niin että myöhemmin voidaan lajitella lähtöajan mukaan ennen taulukon tulostamista
+            var junat = [];
             
+            //käydään läpi jokaisen juna yksitellen ja haetaan halutut tiedot
             for (var i = 0; i < junaData.length; i++){
                 var junaTiedot = junaData[i];
                 var trainType = junaTiedot.trainType;
-                console.log(trainType);
                 var trainNumber = junaTiedot.trainNumber;
+                //junakategoriat tallennetaan suomenkielisiksi
                 var trainCategory = junaTiedot.trainCategory;
                 if (trainCategory === "Commuter") {
                     trainCategory = "Lähijuna";
@@ -80,23 +95,60 @@ function haeAsemanJunat(lyhenne) {
                 } else if (trainCategory === "Shunting"){
                     trainCategory = "Siirrossa";
                 }
-                var scheduledTime = null;
-                var viimeinenAsema = null;
 
+                //alustetaan muuttujia
+                var scheduledTime = null;
+                var asemaKokoNimi = null;
+
+
+                //käydään läpi junan aiktaulutietoja, jotta löydetään tieto kellonajasta, milloin lähtee halutulta asemalta
                 for (var j = 0; j < junaTiedot.timeTableRows.length; j++) {
                     var asema = junaTiedot.timeTableRows[j];
-                    if (asema.stationShortCode === "HKI"){
-                        //Apista tuleva aika on UTC-aikavyöhykkeen mukainen ja halusin tulostuksen suomen aikaa ja vain tunnit ja minuutit
+                    //Apista tuleva aika on UTC-aikavyöhykkeen mukainen ja halusin tulostuksen suomen aikaa ja vain tunnit ja minuutit
+                    if (asema.stationShortCode === lyhenne){
                         var scheduledTimeUTC = asema.scheduledTime;
                         var scheduledTimeLocal = new Date(scheduledTimeUTC);
-                        scheduledTime = ("0" + scheduledTimeLocal.getHours()).slice(-2) + ":" + ("0" + scheduledTimeLocal.getMinutes()).slice(-2);
-
+                        //muodostetaan haluttu tulostusmuoto ajalle, esim. jos tunti tai minuutti on ilmaistu yhtenä numerona, niin lisätään 0
+                        scheduledTime = ("0" + scheduledTimeLocal.getHours()).slice(-2) + ":" + ("0" + scheduledTimeLocal.getMinutes()).slice(-2); 
                     }
-                    viimeinenAsema = asema.stationShortCode;
+
+                    //käydään läpi aikataulutiedot pääteaseman hakemiseksi, viimeisin asema on pääteasema
+                    for (var k = 0; k < asemaNimet.length; k++) {
+                        //koska junien aikataulutiedoissa vaan lyhenteet, niin haetaan aikaisemmin tallennetusta asematiedosta lyhenteen avulla aseman koko nimi
+                        if (asema.stationShortCode === asemaNimet[k].stationShortCode) {
+                            asemaKokoNimi = asemaNimet[k].stationName;
+                            //koska joissakin asemissa (lähinnä Helsinki asema) oli perässä asema, niin poistetaan se
+                            if (asemaKokoNimi.endsWith("asema")) {
+                                asemaKokoNimi = asemaKokoNimi.slice(0, -5);
+                            }
+                            break;
+                        }
+                    }
+
                 }
-                    
-                taulukko += "<tr><td>"  + scheduledTime + "</td><td>" +
-                trainCategory + "<br>" + trainType + " " + trainNumber + "</td><td>" + viimeinenAsema + "</td></tr>";
+                
+                //tallennetaan junan tiedot aiemmin alustettuun junat-listaan
+                junat.push({
+                    scheduledTime: scheduledTime,
+                    trainType: trainType,
+                    trainNumber: trainNumber,
+                    trainCategory: trainCategory,
+                    asemaKokoNimi: asemaKokoNimi
+                });
+                //järjestetään junat lähtöajan mukaan 
+                junat.sort((a, b) => {
+                    var today = new Date().toISOString().slice(0, 10);
+                    var timeA = new Date(today + "T" + a.scheduledTime);
+                    var timeB = new Date(today + "T" + b.scheduledTime);
+                    return timeA - timeB;
+                });
+
+                //luodaan taulu, jonne tulostetaan jokaisen junan tiedot
+                var taulukko = "<table border='1'><tr><th>Lähtöaika</th><th>Juna</th><th>Junan päämäärä</th></tr>";
+                junat.forEach(juna => {
+                    taulukko += "<tr><td>"  + juna.scheduledTime + "</td><td>" +
+                    juna.trainCategory + "<br>" + juna.trainType + " " + juna.trainNumber + "</td><td>" + juna.asemaKokoNimi + "</td></tr>";
+                });
             }
             taulukko += "</table>";
             document.getElementById("junaTaulu").innerHTML = taulukko;   
@@ -107,7 +159,7 @@ function haeAsemanJunat(lyhenne) {
 
 
 
-//Säätietojen hakeminen, workshopista hyödynnetty
+//Säätietojen hakeminen, workshopista hyödynnetty ja halusin myös tässä tulostaa tuo ikonin, jota en itsenäisessä harjoituksessa tajunnut
 
 
 function haeSaa(sijainti) {
@@ -124,18 +176,15 @@ function haeSaa(sijainti) {
         if (this.readyState == 4 && this.status == 200) {
             var weatherJson = getweather.responseText;
             var weatherInfo = JSON.parse(weatherJson);
+            console.log(weatherInfo);
 
-            var saaTiedot = {
-                main: weatherInfo.weather[0].main,
-                temperature: weatherInfo.main.temp,
-                clouds: weatherInfo.clouds.all,
-                humidity: weatherInfo.main.humidity,
-                place: weatherInfo.name
-            };
+            var tulostus = "<table id=weather>";
+            var temperature = weatherInfo.main.temp;
+            var celsius = (temperature - 273.15).toFixed(1);
+            var icon = weatherInfo.weather[0].icon;
+            var newSrc = "https://openweathermap.org/img/w/" + icon + ".png";
+            tulostus += "<tr><td>" + celsius + "°C" + "</td><td>" + "<img src='" + newSrc + "'></img></td></tr>";
 
-            var celsius = (saaTiedot.temperature - 273.15).toFixed(1);
-
-            var tulostus = "Location: " + saaTiedot.place + "<br>Weather: " + saaTiedot.main + "<br>Temperature: " + celsius + "°C<br>Cloudy: " + saaTiedot.clouds + "%<br>Humidity: " + saaTiedot.humidity + "%<br>";
 
             document.getElementById("weatherData").innerHTML = tulostus;
 
@@ -166,7 +215,6 @@ function findLocation(asema) {
                     lon: locationData[0].lon
                 };
 
-                console.log(sijainti);
                 haeSaa(sijainti);     
             } else {
                 alert("Asemalle ei löytynyt säätietoja");
